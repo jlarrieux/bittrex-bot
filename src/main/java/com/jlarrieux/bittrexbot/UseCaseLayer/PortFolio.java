dfebbc44ad14e6be13d277407dbacd49a1b602ff
Jeannius <jeanrodneylarrieux@gmail.com>
2017-09-23 19:57:00 -0500

    implemented portfolio value and portfolio p and l. Added both to MyController

--- src/main/java/com/jlarrieux/bittrexbot/UseCaseLayer/PortFolio.java
+++ src/main/java/com/jlarrieux/bittrexbot/UseCaseLayer/PortFolio.java
@@ -4,7 +4,10 @@ package com.jlarrieux.bittrexbot.UseCaseLayer;
 
 import com.jlarrieux.bittrexbot.Entity.Position;
 import com.jlarrieux.bittrexbot.Entity.Positions;
+import com.jlarrieux.bittrexbot.UseCaseLayer.Adapter.BalanceAdapter;
+import com.jlarrieux.bittrexbot.UseCaseLayer.Adapter.MarketSummaryAdapter;
 import com.jlarrieux.bittrexbot.UseCaseLayer.Manager.PositionManager;
+import com.jlarrieux.bittrexbot.Util.Constants;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -16,22 +19,22 @@ import java.util.List;
 @Component
 public class PortFolio {
 
-
-
     private Positions positionBooks = new Positions();
-    private double total;
+    private MarketSummaryAdapter marketSummaryAdapter;
+    private double total, initialFirstValue;
     private PositionManager positionManager;
 
 
+
+
     @Autowired
-    public PortFolio( PositionManager manager){
-
+    public PortFolio(PositionManager manager, MarketSummaryAdapter marketSummaryAdapter, BalanceAdapter balanceAdapter){
         this.positionManager = manager;
-
+        this.marketSummaryAdapter = marketSummaryAdapter;
+        initialFirstValue = balanceAdapter.getBalance(Constants.BTC.toUpperCase());
     }
 
     public double getCurrentPortFolioValue(){
-
         total=0;
         buildPositionBooks();
         return  total;
@@ -51,40 +54,24 @@ public class PortFolio {
 
 
     private void calculateTotal(){
-//        log.info("Starting portfolio calculation...");
-        int i=1;
         for(String key: positionBooks.keySet()){
-//            log.info(String.format("%d out of %d", i, positionBooks.size()));
             Position p = positionBooks.get(key);
             lowLevelCalculation(p);
-            i++;
         }
-//        log.info("Done calculating portfolio");
+
     }
 
 
     private void lowLevelCalculation(Position p){
-//        JsonObject jsonObject = getMarketSummary(p.getMarketName());
-//        StringBuilder builder;
-//        if(jsonObject!=null ){
-//                if(!jsonObject.get(Constants.BID).toString().equals("null")) {
-//                    double last = JsonParserUtil.getDoubleFromJsonObject(jsonObject, Constants.LAST);
-//                    double bid = JsonParserUtil.getDoubleFromJsonObject(jsonObject, Constants.BID);
-//                    double ask = JsonParserUtil.getDoubleFromJsonObject(jsonObject, Constants.ASK);
-//
-//                    double current = p.getQuantity() * last;
-//                    total += current;
-////                log.info(String.format("Coin: %s\t  with quantity: %f\t current : %f\t and last: %f \t and bid: %f \t and ask: %f \toverall total: %f", Constants.addSpace(p.getCurrency()), p.getQuantity(), current, last, bid, ask, total));
-//                }
-//        }
-//        else{
-////            double balance = JsonParserUtil.getDoubleFromJsonObject(jsonObject,Constants.BALANCE);
-//            total+=p.getQuantity();
-////            log.info(String.format("Coin: %s\t with quantity: %f\t current: \tand overall total: %f", Constants.addSpace(p.getCurrency()), p.getQuantity(),total));
-//        }
-
-
+        marketSummaryAdapter.executeMarketSearch(Constants.buildBtcMarketName(p.getCurrency()));
+        double current = p.getQuantity() * marketSummaryAdapter.getLast();
+        total += current;
     }
+
+    public double profitAndLossPercentage(){
+        return (total-initialFirstValue)/initialFirstValue;
+    }
+
 
 
 
